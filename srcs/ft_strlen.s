e8402d3fb309d3fe10c0dda279f5f829229747b1
PliegerNicolas <plieger05@gmail.com>
2024-05-11 17:04:23 +0200

    Setup ft_strlen tests

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -14,6 +14,7 @@ global ft_strlen                    ; entry-point for linker.
         movdqu      xmm0, [rdi]         ; Move 16-bytes (128-bits) of dereferenced RSI to XMM0.
         add         rdi, 16             ; Move RDI 16 bytes forward for next loop.
 
+        pxor        xmm1, xmm1          ; Set XMM1 to 0 through XOR operation.
         pcmpeqb     xmm1, xmm0          ; Compare XMM0 with XMM1, byte/byte. If equal XMM1 is filled with 0xFF, else 0x00
         
         pmovmskb    ecx, xmm1           ; Move the MSB of each byte to ECX. ECX is 32bytes so only the 16 LSB are affected, the rest is set to 0.
--- srcs/tester/ft_strlen_test.c
+++ srcs/tester/ft_strlen_test.c
@@ -1,7 +1,7 @@
 #include "libasm.h"
 #include "libasm_tester.h"
 
-static void    test_speed(const char *s, size_t (*f)(const char *))
+static void    ft_test_speed(const char *s, size_t (*f)(const char *))
 {
 	struct	timespec	start_time, end_time;
 	double	elapsed_time, average_elapsed_time;
@@ -23,27 +23,82 @@ static void    test_speed(const char *s, size_t (*f)(const char *))
 	printf("Average elapsed time: %f ms\n", average_elapsed_time);
 }
 
+static void	test_speed()
+{
+	printf("%sTest_speed%s\n", GREEN, RESET_COLOR);
+
+	ft_test_speed("Skrattar du, förlorar du, mannen !", strlen);
+	ft_test_speed("Skrattar du, förlorar du, mannen !", ft_strlen);
+}
+
+static void	test_empty_string()
+{
+	printf("%sTest_empty_string%s\n", GREEN, RESET_COLOR);
+
+	printf("%ld\n", strlen(""));
+	printf("%ld\n", ft_strlen(""));
+}
+
+static void	test_one_character()
+{
+	printf("%sTest_one_character%s\n", GREEN, RESET_COLOR);
+
+	printf("%ld\n", strlen("a"));
+	printf("%ld\n", ft_strlen("a"));
+}
+
+static void	test_hellow_world()
+{
+	printf("%sTest_hello_world%s\n", GREEN, RESET_COLOR);
+
+	printf("%ld\n", strlen("a"));
+	printf("%ld\n", ft_strlen("a"));
+}
+
+static void	test_middle_null_char()
+{
+	printf("%sTest_middle_null_char%s\n", GREEN, RESET_COLOR);
+
+	printf("%ld\n", strlen("Hello\0world"));
+	printf("%ld\n", ft_strlen("Hello\0world"));
+}
+
+static void	test_end_null_char()
+{
+	printf("%sTest_end_null_char%s\n", GREEN, RESET_COLOR);
+
+	printf("%ld\n", strlen("This is a test\0"));
+	printf("%ld\n", ft_strlen("This is a test\0"));
+}
+
+static void	test_special_characters()
+{
+	printf("%sTest_special_characters%s\n", GREEN, RESET_COLOR);
+
+	printf("%ld\n", strlen("Special characters: !@#$%^&*()"));
+	printf("%ld\n", ft_strlen("Special characters: !@#$%^&*()"));
+}
+
+static void	test_long_string()
+{
+	printf("%sTest_long_string%s\n", GREEN, RESET_COLOR);
+
+	printf("%ld\n", strlen("This is a very long string to test the efficiency of the strlen function. It should return the correct length regardless of the string's length."));
+	printf("%ld\n", ft_strlen("This is a very long string to test the efficiency of the strlen function. It should return the correct length regardless of the string's length."));
+}
+
 void    test_ft_strlen()
 {
 	printf("%sTest_ft_strlen%s\n", YELLOW, RESET_COLOR);
 
-	test_speed("Skrattar du, förlorar du, mannen !", strlen);
-	test_speed("Skrattar du, förlorar du, mannen !", ft_strlen);
-
-	printf("%ld\n", strlen("Skrattar du, förlorar du, mannen !"));
-	printf("%ld\n", ft_strlen("Skrattar du, förlorar du, mannen !"));
-
-	printf("%ld\n", strlen("oinvdasnoviasdnvasooiv"));
-	printf("%ld\n", ft_strlen("oinvdasnoviasdnvasooiv"));
-
-	printf("%ld\n", strlen("422"));
-	printf("%ld\n", ft_strlen("422"));
-
-	printf("%ld\n", strlen(""));
-	printf("%ld\n", ft_strlen(""));
-
-	printf("%ld\n", strlen("asdgiasdgoinoivasndccoinasodgewtpqowoeptjdaslnvagoisdut90342hiewfaosifdo"));
-	printf("%ld\n", ft_strlen("asdgiasdgoinoivasndccoinasodgewtpqowoeptjdaslnvagoisdut90342hiewfaosifdo"));
+	test_speed();
+	test_empty_string();
+	test_one_character();
+	test_hellow_world();
+	test_middle_null_char();
+	test_end_null_char();
+	test_special_characters();
+	test_long_string();
 
 	printf("\n");
 }
